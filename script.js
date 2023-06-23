f04a14587c5f86e3f58d1653ac0d8ae07a8809a7
JSamper <60151922+jorgesamper@users.noreply.github.com>
2023-06-23 22:25:51 +0200

    logic simple script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 var audio = document.getElementById("audio");
 var playPauseBTN = document.getElementById("playPauseBTN");
-var playIcon = document.getElementById("playIcon");
-var pauseIcon = document.getElementById("pauseIcon");
-var isPlaying = false;
 
-function playPause(){
-	if (!isPlaying) {
-	    isPlaying = true;
-	    audio.play();
-	    playIcon.classList.add("hidden");
-	    pauseIcon.classList.remove("hidden");
-	} else {
-	    isPlaying = false;
-	    audio.pause();
-	    playIcon.classList.remove("hidden");
-	    pauseIcon.classList.add("hidden");
+function playPause() {
+      if (audio.paused) {
+        audio.play();
+        playPauseBTN.innerHTML = '<i class="bi bi-pause-circle"></i>';
+      } else {
+        audio.pause();
+        playPauseBTN.innerHTML = '<i class="bi bi-play-circle"></i>';
 	}
 }
